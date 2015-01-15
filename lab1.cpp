4bb5c4da9b1884c14c65d1c23ba8c412f64d5b3c
Alex Robbins <robbinsa@wit.edu>
2015-01-15 12:51:08 -0500

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //I still dont fully understand the argc and argv or if they're necesarry...
 {
 	const char * c_string; //git bash won't open input file with name as a regular string. use this instead
 	string name;
